1966b4f438c732468949dd3bd84a59130df6a9f3
ArtElephantB <87944666+artelephantb@users.noreply.github.com>
2025-08-13 16:46:26 -0400

    Added chat function

--- create.py
+++ create.py
@@ -2,7 +2,13 @@ from contextlib import contextmanager
 import compile
 
 class CreateFunctions:
-	def __init__(self):
+	def __init__(self, display_name: str, description: str, author: str, format: int, credit_overide=''):
+		self.display_name = display_name
+		self.description = description
+		self.author = author
+		self.format = format
+		self.credit = credit_overide
+
 		self.statements = []
 		self.catagories = []
 		self.current = []
@@ -24,14 +30,19 @@ class CreateFunctions:
 		finally:
 			self.catagories.append({'format':format,'file':name,'content':self.statements})
 			self.statements = []
+	
+	def Chat(self, message: str, target='ap', selector=[]):
+		'''Add tag to entity'''
+		structure = {'function':'chat','input':{'message':message,'target':target,'selector':selector}}
+		self.statements.append(structure)
 
-	def export(self, overide=False):
-		print(self.data)
+	def export(self, location: str, overide=False):
+		compile.CompileData(self.display_name, self.description, self.author, self.format, self.data, credit_overide=self.credit).export(location, overide=overide)
 
-generate = CreateFunctions()
+generate = CreateFunctions('My datapack', 'Example', 'Me', 81)
 
 with generate.Namespace('my-namespace'):
 	with generate.Catagory('my-function', 'function'):
-		pass
+		generate.Chat('Hello!')
 
-generate.export()
+generate.export('output', overide=True)
